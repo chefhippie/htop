8605a8f800967aadc4c9a555187bd044cb2e173b
Thomas Boerger <tboerger@tbpro.de>
2013-10-17 16:59:39 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name "htop"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures htop"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+recipe "htop", "Installs/Configures htop"
+
+supports "suse"
