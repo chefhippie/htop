d5a03706972a4848b3139af6cc4bc9eb981b4938
Thomas Boerger <tboerger@tbpro.de>
2013-12-28 00:03:10 +0100

    Fixed typo for zypper repo

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,7 +22,7 @@ default["htop"]["packages"] = %w(
   nmon
 )
 
-default["htop"]["zypper"]["title"] = "network-utilities"
+default["htop"]["zypper"]["alias"] = "network-utilities"
 default["htop"]["zypper"]["title"] = "Network Utilities"
 default["htop"]["zypper"]["repo"] = "http://download.opensuse.org/repositories/network:/utilities/openSUSE_#{node["platform_version"] == "12.1" ? "12.3" : node["platform_version"]}/"
 default["htop"]["zypper"]["key"] = "#{node["htop"]["zypper"]["repo"]}repodata/repomd.xml.key"
